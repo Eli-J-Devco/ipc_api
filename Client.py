879945f92dd50b9cb0e3649c51822d71e0202c83
MrDuybinh2111 <143157072+MrDuybinh2111@users.noreply.github.com>
2023-09-12 11:23:59 +0700

    12_9_2023_login

--- Client.py
+++ Client.py
@@ -1,71 +1,65 @@
-# /********************************************************
-# * Copyright 2023 NEXT WAVE ENERGY MONITORING INC.
-# * All rights reserved.
-# *
-# *********************************************************/ 
-
-import requests
-import binascii
 import bcrypt
 from cryptography.fernet import Fernet
-import mysql.connector
+import binascii
+import requests
 
- # Thay đổi URL thành URL thực tế của server
-server_url = "http://127.0.0.1:8000/login"
-# Giá trị input bạn muốn gửi lên server
-input_user= input("Nhập giá trị muốn gửi:")
-input_passwword = input("Nhập giá trị muốn gửi:")
+# Change the URL to the actual server URL
+login_server_url = "http://127.0.0.1:8000/login"
+register_server_url = "http://127.0.0.1:8000/register"
 
-# kết nối với sql 
-connection = mysql.connector.connect(
-        user="root",
-        password="123456",
-        host="localhost",
-        database="login",
-    )
-    # Lấy tất cả dữ liệu từ bảng data
-cursor = connection.cursor()
-commadsql = ("SELECT salt FROM login.data WHERE user = %s")
-val = (input_user,)
-cursor.execute(commadsql, val)
-result = cursor.fetchall()
-if result:
-    tuplesalt = result[0]
-    salt_hex = tuplesalt[0]
-    print("mã salt hex lấy lên từ sql là :",salt_hex)
-    saltfull = binascii.unhexlify(salt_hex)
-    print("salt lấy lên từ sql: ", saltfull)
-else:
-    print("Không tìm thấy thông tin người dùng.")
-# mỗi lần tạo salt thì mật khẩu sẽ khác nhau nên muốn cùng salt thì phải gửi lun mã salt này vào sql 
-password_lv1 = bcrypt.hashpw(input_passwword.encode(), saltfull)
-print(" Mật khẩu băm qua bcrypt", password_lv1)
-
-# Tạo một khóa ngẫu nhiên cho Fernet
+# Encrypt information with two layers
+# Encrypt the password in step 1
+# Generate a random salt
+salt = bcrypt.gensalt()
+# Use the generated salt to hash the password at level 1
+# Generate a key to create a random key for Fernet
 fernet_key = Fernet.generate_key()
-client_key = Fernet(fernet_key)
-print("đây là ramdom key :",client_key)
-# Mã hóa mật khẩu bcrypt với khóa Fernet
-password_lv2 = client_key.encrypt(password_lv1)
+# Create a random key
+key = Fernet(fernet_key)
+# Use the generated Fernet key to encrypt the password at level 2
 
-print( "Mật khẩu băm qua fernet :" , password_lv2)
+# Convert salt and key to hex format for transmission
+salt_hex = binascii.hexlify(salt).decode()
+key_hex = binascii.hexlify(fernet_key).decode()
 
-# Chuyển đổi dữ liệu bytes thành chuỗi hex
-password_lv2_hex = binascii.hexlify(password_lv2).decode()
-print("Mã đã chuyển qua hex ",password_lv2_hex)
+# Send account information, password, password salt, and Fernet password
+def register():
+    # Values for a row provided as a tuple
+    username_input = input("Enter a new username:")
+    password_input = input("Enter a new password:")
+    # Use the generated salt to hash the password at level 1
+    password_lv1 = bcrypt.hashpw(password_input.encode(), salt)
+    password_lv2 = key.encrypt(password_lv1)
+    # Create a dictionary containing input values
+    payload = {"username_input": username_input, "password_input": password_lv2, "salt": salt_hex, "key": key_hex}
+    # Encode client_key_hex back to bytes and then create client_key = Fernet(fernet_key) to get the actual key
 
-# Chuyển đổi dữ liệu key bcrypt thành key hex để gửi đi 
-client_key_hex = binascii.hexlify(fernet_key).decode()
-print("Key đã chuyển qua hex ",client_key_hex)
+    # Send a POST request to the FastAPI server with JSON data
+    response = requests.post(register_server_url, json=payload)
 
-# Tạo một dictionary chứa giá trị input // payload này tên như thế nào thì class InformationUser(BaseModel):/client_user: str/client_password: str tên như thế mới kết nối được 
-payload = {"client_user": input_user,"client_password": password_lv2_hex,"client_key":client_key_hex}
-#client_key_hex => mã hóa lại byte => client_key = Fernet(fernet_key) để lấy key thật 
+# Send account and password information
+# Input values you want to send to the server
+def login():
+    username_input = input("Enter your username:")
+    password_input = input("Enter your password:")
+    # Use the generated salt to hash the password at level 1
+    password_lv1 = bcrypt.hashpw(password_input.encode(), salt)
+    password_lv2 = key.encrypt(password_lv1)
+    # Create a dictionary containing input values
+    payload = {"username_input": username_input, "password_input": password_lv2}
+    # Encode client_key_hex back to bytes and then create client_key = Fernet(fernet_key) to get the actual key
 
-# Gửi yêu cầu POST đến server FastAPI với dữ liệu JSON
-response = requests.post(server_url, json=payload)
-# Kiểm tra phản hồi từ server
-# if response.status_code == 200:
-#     print("Đăng nhập thành công!")
-# else:
-#     print("Đăng nhập thất bại. Vui lòng kiểm tra thông tin đăng nhập.")
+    # Send a POST request to the FastAPI server with JSON data
+    response = requests.post(login_server_url, json=payload)
+
+def main():
+    choice = int(input("Enter 0 to register or 1 to login: "))
+    if choice == 0:
+        register()
+    elif choice == 1:
+        login()
+    else:
+        print("Invalid choice")
+
+if __name__ == "__main__":
+    main()
