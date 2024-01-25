668aff5a9c4ca0c417d02da555a2b31f7505e6b2
nguyenvudtd <vnguyen@nwemon.com>
2024-01-25 10:47:26 +0700

    Update test9.py

--- test/test9.py
+++ test/test9.py
@@ -2,6 +2,6 @@ import tkinter as tk
 
 r = tk.Tk()
 r.title('Counting Seconds')
-button = tk.Button(r, text='Stop', width=25, command=r.destroy)
+button = tk.Button(r, text='Stop', width=25,height=20, command=r.destroy)
 button.pack()
 r.mainloop()
