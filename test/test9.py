36b8652fcbf039643fd73d581482d0b4215589b8
nguyenvudtd <vnguyen@nwemon.com>
2024-01-25 10:46:06 +0700

    test build

--- test/test9.py
+++ test/test9.py
@@ -1,2 +1,7 @@
-def main():
-    print('hello')
+import tkinter as tk
+
+r = tk.Tk()
+r.title('Counting Seconds')
+button = tk.Button(r, text='Stop', width=25, command=r.destroy)
+button.pack()
+r.mainloop()
