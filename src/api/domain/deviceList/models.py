e5191be96993d93432ff68f7f93b4a4051bca57c
nguyenvudtd <vnguyen@nwemon.com>
2024-03-26 21:54:18 +0700

    ---

--- src/api/domain/deviceList/models.py
+++ src/api/domain/deviceList/models.py
@@ -7,15 +7,19 @@ import os
 import sys
 from datetime import datetime
 
-from sqlalchemy import (DOUBLE, BigInteger, Boolean, Column, DateTime,
-                        ForeignKey, Integer, String, Text, create_engine)
-from sqlalchemy.orm import (declarative_base, mapped_column, relationship,
-                            sessionmaker)
+from sqlalchemy import (DOUBLE, BigInteger, Boolean, Column, DateTime, Float,
+                        ForeignKey, Integer, MetaData, String, Table, Text,
+                        create_engine)
+from sqlalchemy.orm import (Mapped, declarative_base, mapped_column,
+                            relationship, sessionmaker)
+from sqlalchemy.schema import CreateTable, DropTable
 from sqlalchemy.sql.expression import text
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 
 sys.path.append( (lambda project_name: os.path.dirname(__file__)[:len(project_name) + os.path.dirname(__file__).find(project_name)] if project_name and project_name in os.path.dirname(__file__) else -1)
                 ("src"))
+from sqlalchemy_utils.view import CreateView
+
 from database.db import Base, engine
 
 
@@ -134,3 +138,31 @@ class Data_panel(Base):
         "device_panel.id", ondelete="CASCADE", onupdate="CASCADE"), nullable=True)
    
     device_panel = relationship('Device_panel', foreign_keys=[id_device_panel])
+
+def create_table_device(table_name):
+    TABLE_SPEC = [
+                    ('id', BigInteger),
+                    ('name', String(255)),
+                    ('time',DateTime),
+                    ('whatever', String(255)),
+                    ("employee_dept", Integer, ForeignKey("departments.department_id"))
+                ]
+
+    TABLE_NAME = 'sample_table'
+
+    columns = [Column(n, t,t1) for n, t,t1 in TABLE_SPEC]
+    table = Table(TABLE_NAME, MetaData(), *columns)
+    table_creation_sql = CreateTable(table)
+def create_table(tbl_name, number=10):
+    print("create_table")
+    class CreateTables(Base):
+        __table_args__ = {'extend_existing': True} 
+        __tablename__ = f"{tbl_name}"
+        id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+        name: Mapped[str] = mapped_column(String(16), unique=True)
+    tbl = CreateTables.__table__
+    for col in range(number):
+        tbl._columns.add(Column(f"pt{col}", Integer))
+    result=CreateView(tbl)
+    return result
+    # return CreateTable.__table__
--- src/database/db.py
+++ src/database/db.py
@@ -1,18 +1,25 @@
+import asyncio
 import os
 import sys
-from datetime import datetime
-
 # path=path_directory_relative("ipc_api") # name of project
 # sys.path.append(path)
 # #
+from contextlib import asynccontextmanager
+from datetime import datetime
+
 import pymysql
 import pymysql.cursors
 import sqlalchemy as sa
 from pymysql.constants import CLIENT
 from sqlalchemy import (DOUBLE, BigInteger, Boolean, Column, DateTime,
                         ForeignKey, Integer, String, Text, create_engine)
-from sqlalchemy.orm import (declarative_base, mapped_column, relationship,
-                            sessionmaker)
+from sqlalchemy.ext.asyncio import (AsyncSession, async_sessionmaker,
+                                    create_async_engine)
+# from sqlalchemy.orm import (declarative_base, mapped_column, relationship,
+#                             sessionmaker)
+# from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import (DeclarativeBase, declarative_base, mapped_column,
+                            relationship, sessionmaker)
 from sqlalchemy.pool import QueuePool
 from sqlalchemy.sql.expression import text
 from sqlalchemy.sql.sqltypes import TIMESTAMP
@@ -31,34 +38,17 @@ database_username =Config.DATABASE_USERNAME
 # print(f'database_password: {database_password}')
 # print(f'database_name: {database_name}')
 # print(f'database_username: {database_username}')
+# -------------------------------------------------------------------------------------
 SQLALCHEMY_DATABASE_URL = f'mysql+pymysql://{database_username}:{database_password}@{database_hostname}:{database_port}/{database_name}'
-
-
-
 engine = create_engine(SQLALCHEMY_DATABASE_URL,   
                        isolation_level="READ UNCOMMITTED",
                        pool_size=20, 
                        max_overflow=100,
                        )
-# def get_pymysql_connection():
-#     return pymysql.connect(
-#         host=database_hostname,
-#         port=int(database_port),
-#         user=database_username,
-#         password=database_password,
-#         database=database_name,
-#         client_flag=CLIENT.MULTI_STATEMENTS,
-#          charset='utf8mb4',
-#          max_allowed_packet=16 * 1024 * 1024,
-#          connect_timeout=10,
-#         #   client_flag=0,
-#         # cursorclass=pymysql.cursors.DictCursor
-#     )
-# engine = sa.create_engine("mysql+pymysql://", creator=get_pymysql_connection)
 # 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-# db = SessionLocal()
+# @asynccontextmanager
 def get_db():
     db = SessionLocal()
     try:
@@ -67,3 +57,22 @@ def get_db():
             db.rollback()
     finally:
         db.close()
+# class Base(DeclarativeBase):
+#     pass
+# SQLALCHEMY_DATABASE_URL = f'mysql+aiomysql://{database_username}:{database_password}@{database_hostname}:{database_port}/{database_name}'
+# engine = create_async_engine(SQLALCHEMY_DATABASE_URL
+#                             #  ,connect_args={"check_same_thread":False}
+#                        )
+# SessionLocal = async_sessionmaker(engine)
+# # Base = declarative_base()
+# async def get_db():
+#     async with engine.begin() as conn:
+#         await conn.run_sync(Base.metadata.create_all)
+#     db = SessionLocal()
+#     try:
+#         yield db
+#     except Exception as err:
+#             await db.rollback()
+#     finally:
+#         await db.close()
+
