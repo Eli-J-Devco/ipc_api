6a91d8fb3872f2af928571f77d017446fdb62c2b
nartnahnnw <nhantran@nwemon.com>
2024-05-11 20:52:14 +0700

    update build

--- build/build.sh
+++ build/build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-echo 123654789 | su -l ipc
-echo "$USER"
-cp -rf /var/lib/jenkins/workspace/api_python_new/ /home/ipc/sources/api_python_new/
-cd /home/ipc/sources/api_python_new/ || exit
+if [ -d /home/jenkins/sources/ ]; then echo 'Exists'; else mkdir "/home/jenkins/sources/"; fi
+cp -rf /var/lib/jenkins/workspace/api_python_new/ /home/jenkins/sources/api_python_new/
+cd /home/jenkins/sources/api_python_new/ || exit
 python3 -m venv venv
 source venv/bin/activate
 python3 -m pip install -r requirements.txt  --no-cache-dir
+cp .env.example .env
+source venv/bin/activate
+python3 main.py
