cdf0ec08e9b589142e9feb3e254d4863dee7bfbf
nartnahnnw <nhantran@nwemon.com>
2024-05-11 21:39:53 +0700

    Fix dir error

--- build/build.sh
+++ build/build.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-su -c ipc
-echo "$USER"
-rm -rf /home/ipc/sources/api_python_new/
-cp -rf /var/lib/jenkins/workspace/api_python_new/ /home/ipc/sources/api_python_new/
-cd /home/ipc/sources/api_python_new/ || exit
+rm -rf /home/jenkins/sources/api_python_new/
+cp -rf /var/lib/jenkins/workspace/api_python_new/ /home/jenkins/sources/api_python_new/
+cd /home/jenkins/sources/api_python_new/ || exit
 python3 -m venv venv
 source venv/bin/activate
 python3 -m pip install -r requirements.txt  --no-cache-dir
