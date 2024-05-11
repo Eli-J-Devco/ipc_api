d4107f6ec6efdfd8894e83cb60b8de74ec900a54
nartnahnnw <nhantran@nwemon.com>
2024-05-11 21:11:51 +0700

    copy env

--- build/build.sh
+++ build/build.sh
@@ -6,6 +6,5 @@ cd /home/jenkins/sources/api_python_new/ || exit
 python3 -m venv venv
 source venv/bin/activate
 python3 -m pip install -r requirements.txt  --no-cache-dir
-cp .env.example .env
+cp /ipc_api/.env.example /ipc_api/.env
 source venv/bin/activate
-python3 main.py
