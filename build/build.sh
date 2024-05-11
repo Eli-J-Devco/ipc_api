b1c899274442476a88f2bb4d987f18774682c0c3
nartnahnnw <nhantran@nwemon.com>
2024-05-11 21:19:31 +0700

    change env path

--- build/build.sh
+++ build/build.sh
@@ -6,5 +6,4 @@ cd /home/jenkins/sources/api_python_new/ || exit
 python3 -m venv venv
 source venv/bin/activate
 python3 -m pip install -r requirements.txt  --no-cache-dir
-cp /ipc_api/.env.example /ipc_api/.env
-source venv/bin/activate
+cp ./ipc_api/.env.example ./ipc_api/.env
