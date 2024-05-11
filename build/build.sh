f06113d0cf0f93ad52448c361b07128b0c82ed85
nartnahnnw <nhantran@nwemon.com>
2024-05-11 21:37:55 +0700

    clear setup dir

--- build/build.sh
+++ build/build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-if [ -d /home/jenkins/ ]; then echo 'Exists'; else mkdir "/home/jenkins/"; fi
-if [ -d /home/jenkins/sources/ ]; then echo 'Exists'; else mkdir "/home/jenkins/sources/"; fi
-cp -rf /var/lib/jenkins/workspace/api_python_new/ /home/jenkins/sources/api_python_new/
-cd /home/jenkins/sources/api_python_new/ || exit
+su -c ipc
+echo "$USER"
+rm -rf /home/ipc/sources/api_python_new/
+cp -rf /var/lib/jenkins/workspace/api_python_new/ /home/ipc/sources/api_python_new/
+cd /home/ipc/sources/api_python_new/ || exit
 python3 -m venv venv
 source venv/bin/activate
 python3 -m pip install -r requirements.txt  --no-cache-dir
