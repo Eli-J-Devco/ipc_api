f38ad59d04a4c93bb1991ce5008182bc1956a604
nartnahnnw <nhantran@nwemon.com>
2024-05-11 21:00:37 +0700

    Verify output when build

--- build/build.sh
+++ build/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+if [ -d /home/jenkins/ ]; then echo 'Exists'; else mkdir "/home/jenkins/"; fi
 if [ -d /home/jenkins/sources/ ]; then echo 'Exists'; else mkdir "/home/jenkins/sources/"; fi
 cp -rf /var/lib/jenkins/workspace/api_python_new/ /home/jenkins/sources/api_python_new/
 cd /home/jenkins/sources/api_python_new/ || exit
