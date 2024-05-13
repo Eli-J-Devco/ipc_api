b40ded72e10314b80e9d89f995d6c32e6b3b80c5
nartnahnnw <nhantran@nwemon.com>
2024-05-13 17:09:14 +0700

    Update env

--- src/pm2_service/pm2_service.py
+++ src/pm2_service/pm2_service.py
@@ -10,14 +10,14 @@ from ..config import config
 
 class PM2Service:
     def __init__(self, serial_number: str):
-        self.sender = Publisher(host=config.PM2_MQTT_HOST,
-                                port=config.PM2_MQTT_PORT,
+        self.sender = Publisher(host=config.MQTT_HOST,
+                                port=config.MQTT_PORT,
                                 subscriptions=["#"],
-                                username=config.PM2_MQTT_USERNAME,
-                                password=config.PM2_MQTT_PASSWORD.encode("utf-8"),
-                                client_id=f"publisher-{config.PM2_MQTT_CLIENT_ID}-{uuid.uuid4()}",
-                                will_qos=config.PM2_MQTT_QOS,
-                                will_retain=config.PM2_MQTT_RETAIN)
+                                username=config.MQTT_USERNAME,
+                                password=config.MQTT_PASSWORD.encode("utf-8"),
+                                client_id=f"publisher-{config.MQTT_CLIENT_ID}-{uuid.uuid4()}",
+                                will_qos=config.MQTT_QOS,
+                                will_retain=config.MQTT_RETAIN)
         self.serial_number = serial_number
 
     async def send(self, message: MessageModel, resume_session: bool = True):
