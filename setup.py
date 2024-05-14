8bc6c86c0e2f08ca1a8ab598b44ff5c2b07581c7
nartnahnnw <nhantran@nwemon.com>
2024-05-14 11:40:27 +0700

    add setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+import subprocess
+import sys
+from pathlib import Path
+
+PATH = Path(__file__).parent.absolute()
+
+
+def init():
+    absDirname = PATH
+    pid = f'API_V2'
+    if sys.platform == 'win32':
+        # use run with window
+        subprocess.Popen(
+            f'pm2 start {absDirname}/api/main.py -f --name "{pid}" --restart-delay=10000', shell=True).communicate()
+    else:
+        # use run with ubuntu/linux
+        subprocess.Popen(
+            f'sudo pm2 start {absDirname}/main.py --interpreter {absDirname}/venv/bin/python3 -f --name "{pid}" '
+            f'--restart-delay=10000', shell=True).communicate()
+
+
+init()
