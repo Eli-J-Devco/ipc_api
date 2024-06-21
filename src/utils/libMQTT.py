e359788049beeab10c8edbcdadbd8739bfbb9163
duybinh <ndbinhtdh99@gmail.com>
2024-06-21 10:40:14 +0700

    Done

--- src/utils/libMQTT.py
+++ src/utils/libMQTT.py
@@ -18,11 +18,11 @@ import paho.mqtt.publish as publish
 # 	 * @param {host, port,topic, username, password, data_send}
 # 	 * @return data ()
 # 	 */
-def push_data_to_mqtt(host, port,topic, username, password, data_send):
+def push_data_to_mqtt(host, port,topic,username, password, data_send,qos=0,retain=False):
     try:
         payload = json.dumps(data_send)
-        publish.single(topic, payload, hostname=host,
-                    retain=False, port=port,
+        publish.single(topic, payload, hostname=host,qos=qos,
+                    retain=retain, port=port,
                     auth = {'username':f'{username}', 
                             'password':f'{password}'})
         # publish.single(Topic, payload, hostname=Broker,
