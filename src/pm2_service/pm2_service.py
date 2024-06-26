765771d16ed7c37fc6fa293388880e5d2c00506d
nartnahnnw <160694961+nartnahnnw@users.noreply.github.com>
2024-06-26 16:55:26 +0700

    Fix disconnect bug

--- src/pm2_service/pm2_service.py
+++ src/pm2_service/pm2_service.py
@@ -12,7 +12,7 @@ class PM2Service:
     def __init__(self, serial_number: str):
         self.sender = Publisher(host=config.MQTT_HOST,
                                 port=config.MQTT_PORT,
-                                subscriptions=["#"],
+                                subscriptions=[serial_number],
                                 username=config.MQTT_USERNAME,
                                 password=config.MQTT_PASSWORD.encode("utf-8"),
                                 client_id=f"publisher-{config.MQTT_CLIENT_ID}-{uuid.uuid4()}",
@@ -21,12 +21,22 @@ class PM2Service:
         self.serial_number = serial_number
 
     async def send(self, message: MessageModel, resume_session: bool = True):
+        if not self.sender:
+            self.sender = Publisher(host=config.MQTT_HOST,
+                                    port=config.MQTT_PORT,
+                                    subscriptions=[self.serial_number],
+                                    username=config.MQTT_USERNAME,
+                                    password=config.MQTT_PASSWORD.encode("utf-8"),
+                                    client_id=f"publisher-{config.MQTT_CLIENT_ID}-{uuid.uuid4()}",
+                                    will_qos=config.MQTT_QOS,
+                                    will_retain=config.MQTT_RETAIN)
+        await self.sender.stop()
         try:
             await self.sender.start(resume_session=resume_session)
         except SessionResumeError:
             await self.sender.start()
-        finally:
             self.sender.send(f"{self.serial_number}/{config.MQTT_INITIALIZE_TOPIC}",
                              json.dumps(message.dict()).encode("ascii"))
+        finally:
             await self.sender.stop()
 
