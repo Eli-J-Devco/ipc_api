a398c1c3b4e1f8336ad6050809976a8453488bf5
nguyenvudtd <vnguyen@nwemon.com>
2024-07-03 09:18:38 +0700

    Update devices_service.py

--- src/apiGateway/devices/devices_service.py
+++ src/apiGateway/devices/devices_service.py
@@ -311,6 +311,8 @@ class DevicesService:
                 print(f'delete_device_list: {delete_device_list}')
                 await self.mqtt_init.send("Control/Write",
                                 delete_device_list)
+                await self.mqtt_init.send("Control/Writes",
+                                delete_device_list)
         except Exception as e:
             print("Error delete_dev: ", e)
         finally:
