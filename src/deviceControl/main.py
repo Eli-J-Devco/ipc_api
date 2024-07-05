769d7e36d9c778d137e90d6b1973197ac6f63ae5
duybinh <ndbinhtdh99@gmail.com>
2024-07-05 10:37:05 +0700

    Done

--- src/deviceControl/main.py
+++ src/deviceControl/main.py
@@ -622,7 +622,7 @@ async def get_list_device_in_automode(mqtt_result):
 ############################################################################ List Device Systemp ############################################################################
 async def get_list_device_in_process(mqtt_result, serial_number_project, host, port, username, password):
     # Global variables
-    global total_power, MQTT_TOPIC_PUD_LIST_DEVICE_PROCESS,value_consumption,value_production,value_power_limit,system_performance,low_performance , high_performance ,total_wmax_man,total_wmax,ModeSysTemp
+    global total_power, MQTT_TOPIC_PUD_LIST_DEVICE_PROCESS,value_consumption,value_production,value_power_limit,system_performance,low_performance , high_performance ,total_wmax_man,total_wmax,ModeSystempCurrent
     
     # Local variable
     device_list = []
@@ -735,6 +735,7 @@ async def get_list_device_in_process(mqtt_result, serial_number_project, host, p
     total_wmax_man_temp = round(total_wmax_man_temp,1)
     
     result = {
+    "ModeSystempCurrent":ModeSystempCurrent,
     "devices": device_list,
     "total_max_power": total_power + total_wmax_man_temp,
     "system_performance": {
