72dcaa288d760be20d2500c7752d87e14fdbeebd
duybinh <ndbinhtdh99@gmail.com>
2024-07-16 11:29:15 +0700

    Done

--- src/deviceControl/main.py
+++ src/deviceControl/main.py
@@ -1222,15 +1222,13 @@ async def process_update_parameter_mode_detail(mqtt_result,serial_number_project
                 else :
                     value_offset_power_limit = value_offset_power_limit_temp
                 value_power_limit_temp = mqtt_result["value"]
-                
+                print("value_power_limit_temp",value_power_limit_temp)
+                print("total_power",total_power)
                 if value_power_limit_temp is not None :
                     if value_power_limit_temp <= total_power:
-                        value_power_limit = value_power_limit_temp
+                        print("da vao day")
                         # write information in database 
                         if value_power_limit <= total_power :
-                            print("value_power_limit_temp",value_power_limit_temp)
-                            print("total_power",total_power)
-                            
                             result_parameter_power_limit = MySQL_Update_V1("update project_setup set value_power_limit = %s ,value_offset_power_limit = %s ", (value_power_limit_temp,value_offset_power_limit,))
                         # convert value kw to w 
                             value_power_limit = (value_power_limit - (value_power_limit*value_offset_power_limit)/100)
