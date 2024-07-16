9b671b5f646e22db5e1d8b957d22ce46917a9b2d
duybinh <ndbinhtdh99@gmail.com>
2024-07-16 14:59:44 +0700

    Done

--- src/deviceControl/main.py
+++ src/deviceControl/main.py
@@ -1227,37 +1227,36 @@ async def process_update_parameter_mode_detail(mqtt_result,serial_number_project
                 print("value_power_limit_temp",value_power_limit_temp)
                 print("total_power",total_power_systemp)
                 if value_power_limit_temp is not None :
-                    if value_power_limit_temp >= total_power_systemp:
+                    if value_power_limit_temp <= total_power_systemp:
                         value_power_limit = value_power_limit_temp
                         # write information in database 
-                        if value_power_limit >= total_power_systemp:
+                        if value_power_limit <= total_power_systemp:
+                            print("da vao day")
                             result_parameter_power_limit = MySQL_Update_V1("update project_setup set value_power_limit = %s ,value_offset_power_limit = %s ", (value_power_limit_temp,value_offset_power_limit,))
                         # convert value kw to w 
                             value_power_limit = (value_power_limit - (value_power_limit*value_offset_power_limit)/100)
             # When you receive one of the above information, give feedback to mqtt
-            if ( value_offset_zero_export or value_offset_power_limit or value_power_limit ) :
-                if result_parameter_zero_export == None or result_parameter_power_limit == None or (value_power_limit_temp != None and value_power_limit_temp > total_power):
-                    comment = 400 
-                else:
-                    comment = 200 
-                data_send = {
-                            "time_stamp" :current_time,
-                            "status":comment, 
-                            }
-                mqtt_public_paho_zip(mqtt_host,
-                        mqtt_port,
-                        topicPudModeAuto ,
-                        mqtt_username,
-                        mqtt_password,
-                        data_send)
-                push_data_to_mqtt(mqtt_host,
-                        mqtt_port,
-                        topicPudModeAuto + "Binh" ,
-                        mqtt_username,
-                        mqtt_password,
-                        data_send)
+            if result_parameter_zero_export == None or result_parameter_power_limit == None or (value_power_limit_temp != None and value_power_limit_temp > total_power):
+                comment = 400 
             else:
-                pass
+                comment = 200 
+            data_send = {
+                        "time_stamp" :current_time,
+                        "status":comment, 
+                        }
+            mqtt_public_paho_zip(mqtt_host,
+                    mqtt_port,
+                    topicPudModeAuto ,
+                    mqtt_username,
+                    mqtt_password,
+                    data_send)
+            push_data_to_mqtt(mqtt_host,
+                    mqtt_port,
+                    topicPudModeAuto + "Binh" ,
+                    mqtt_username,
+                    mqtt_password,
+                    data_send)
+            
     except Exception as err:
         print(f"Error MQTT subscribe process_update_parameter_mode_detail: '{err}'")
 # Describe process_update_mode_detail 
