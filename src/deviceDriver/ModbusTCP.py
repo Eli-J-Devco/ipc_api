85b21fbb33828226f989be3e11e6cf320cd9b216
duybinh <ndbinhtdh99@gmail.com>
2024-07-18 16:34:15 +0700

    Done

--- src/deviceDriver/ModbusTCP.py
+++ src/deviceDriver/ModbusTCP.py
@@ -1911,7 +1911,7 @@ async def process_sud_control_man(mqtt_result, serial_number_project, host, port
     if mqtt_result and any(int(item.get('id_device')) == int(id_systemp) for item in mqtt_result):
         result_topic1 = mqtt_result
         print("result_topic1",result_topic1)
-        if result_topic1 and bitcheck_topic1 == 1:
+        if result_topic1 and bitcheck_topic1 == 1 :
             # Get value_zero_export and value_power_limit in DB 
             await Get_value_Power_Limit()
             # Update mode temp for Device 
@@ -1933,7 +1933,8 @@ async def process_sud_control_man(mqtt_result, serial_number_project, host, port
                     device["wmax"] = 0
             # Update rated power to the device and check status when saving the device's control parameters to the system
             comment, watt,custom_watt = await updates_ratedpower_from_message(result_topic1,wmax)
-            if result_topic1 and bitcheck_topic1 == 1:
+            # Check have topic and man mode action because message auto a lot of
+            if result_topic1 and bitcheck_topic1 == 1 and device_mode == 0:
                 if comment == 400 :
                     # If the update fails, return the mode value and print an error without doing anything else
                     result_topic1 = []
@@ -1953,6 +1954,8 @@ async def process_sud_control_man(mqtt_result, serial_number_project, host, port
                     MySQL_Update_V1('update `device_list` set `rated_power_custom` = %s, `rated_power` = %s where `id` = %s', (custom_watt, watt, id_systemp))
                     MySQL_Update_V1("UPDATE device_point_list_map dplm JOIN point_list pl ON dplm.id_point_list = pl.id SET dplm.control_max = %s WHERE pl.id_pointkey = 'Wmax' AND dplm.id_device_list = %s", (rated_power_custom_calculator, id_systemp))
                 # reset global value to avoid accumulation
+                custom_watt = 0
+                watt = 0
                 total_wmax_man_temp = 0
 # Describe process_message 
 # 	 * @description processmessage from mqtt
