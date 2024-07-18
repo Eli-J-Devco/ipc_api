fb855b03ca8f151e8b2ca2826a9b34d1d227f946
duybinh <ndbinhtdh99@gmail.com>
2024-07-18 14:55:57 +0700

    Done

--- src/deviceDriver/ModbusTCP.py
+++ src/deviceDriver/ModbusTCP.py
@@ -1055,7 +1055,7 @@ async def write_device(
                                 "time_stamp": current_time,
                                 "status": 200,
                             }
-                    mqtt_public_paho_zip(mqtt_host, mqtt_port, topicPublic, mqtt_username, mqtt_password, data_send)
+                    mqtt_public_paho_zip(mqtt_host, mqtt_port, topicPublic + "/Feedbacksetup", mqtt_username, mqtt_password, data_send)
                     result_topic1 = []
 # Describe functions before writing code
 # /**
