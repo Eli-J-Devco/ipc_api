544bb507e4ec6fec34187d008fabee609ede367e
duybinh <ndbinhtdh99@gmail.com>
2024-07-23 10:47:31 +0700

    Done

--- src/deviceDriver/ModbusTCP.py
+++ src/deviceDriver/ModbusTCP.py
@@ -2003,16 +2003,14 @@ async def process_message(topic, message,serial_number_project, host, port, user
                     result_topic1_Temp = message
                     is_waiting = True
                     await process_sud_control_man(result_topic1_Temp, serial_number_project, host, port, username, password)
-                    await asyncio.sleep(15)
-                    message = []
-                    result_topic1_Temp = []
-                    is_waiting = False  
                     
             elif topic == topic3:
                 if not is_waiting:
                     result_topic3_Temp = message
                     await process_sud_control_man(result_topic3_Temp, serial_number_project, host, port, username, password)
-
+            
+            is_waiting = False  
+            
         elif topic == topic2:
             result_topic2 = message
             # process 
