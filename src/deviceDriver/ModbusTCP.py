557dde9dd12f426cb594d0c96fa2efc3e9229c1f
duybinh <ndbinhtdh99@gmail.com>
2024-07-23 11:31:04 +0700

    Done

--- src/deviceDriver/ModbusTCP.py
+++ src/deviceDriver/ModbusTCP.py
@@ -2001,7 +2001,7 @@ async def process_message(topic, message,serial_number_project, host, port, user
                     result_topic1_Temp = message
                     is_waiting = True
                     await process_sud_control_man(result_topic1_Temp, serial_number_project, host, port, username, password)
-                    await asyncio.sleep(30)
+                    await asyncio.sleep(10)
                     is_waiting = False  
                     message = []
                     result_topic1_Temp = []
