d36391189b6bbd745773015e0b027a089faa748d
duybinh <ndbinhtdh99@gmail.com>
2024-07-24 10:00:26 +0700

    Done

--- src/deviceControl/main.py
+++ src/deviceControl/main.py
@@ -225,6 +225,7 @@ async def get_cpu_information(serial_number_project, mqtt_host, mqtt_port, mqtt_
         system_info["MemoryInformation"]["Total"] = get_readable_size(svmem.total)
         system_info["MemoryInformation"]["Available"] = get_readable_size(svmem.available)
         system_info["MemoryInformation"]["Used"] = get_readable_size(svmem.total - svmem.available)
+        system_info["MemoryInformation"]["Used1"] = get_readable_size(svmem.used)
         system_info["MemoryInformation"]["Percentage"] = f"{svmem.percent:.1f}%"
 
         swap = psutil.swap_memory()
