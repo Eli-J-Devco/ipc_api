e20eea4481044701575efb71d61410573be56d27
duybinh <ndbinhtdh99@gmail.com>
2024-07-24 10:15:29 +0700

    Done

--- src/deviceControl/main.py
+++ src/deviceControl/main.py
@@ -224,7 +224,7 @@ async def get_cpu_information(serial_number_project, mqtt_host, mqtt_port, mqtt_
         svmem = psutil.virtual_memory()
         system_info["MemoryInformation"]["Total"] = get_readable_size(svmem.total)
         system_info["MemoryInformation"]["Available"] = get_readable_size(svmem.available)
-        system_info["MemoryInformation"]["Used"] = get_readable_size(svmem.total - svmem.available)
+        system_info["MemoryInformation"]["Used"] = get_readable_size(svmem.total - svmem.available - svmem.free/10)
         system_info["MemoryInformation"]["Used1"] = get_readable_size(svmem.used)
         system_info["MemoryInformation"]["Used2"] = get_readable_size(svmem.free)
         system_info["MemoryInformation"]["Percentage"] = f"{svmem.percent:.1f}%"
