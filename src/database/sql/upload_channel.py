ef7b5665b102f441d2757578b285a4003d8bd238
nguyenvudtd <vnguyen@nwemon.com>
2024-07-29 13:56:47 +0700

    Update upload_channel.py

--- src/database/sql/upload_channel.py
+++ src/database/sql/upload_channel.py
@@ -35,4 +35,13 @@ class Query():
         INNER JOIN config_information ON config_information.id=upload_channel.id_type_protocol 
         Where  upload_channel.id ={id_upload_channel};
     """
+    select_all_upload_channel="""
+        SELECT 
+        upload_channel.id,upload_channel.name ,
+        config_information.namekey as type_protocol,
+	    upload_channel.enable
+        FROM upload_channel 
+        INNER JOIN config_information ON config_information.id=upload_channel.id_type_protocol 
+        Where  upload_channel.status ={status};
+    """
 all_query=Query()
