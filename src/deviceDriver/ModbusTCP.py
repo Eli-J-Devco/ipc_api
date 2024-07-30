d5399a1b1eff94a585f4dad73e575d3de89baa55
duybinh <ndbinhtdh99@gmail.com>
2024-07-30 10:01:11 +0700

    Done

--- src/deviceDriver/ModbusTCP.py
+++ src/deviceDriver/ModbusTCP.py
@@ -1815,7 +1815,10 @@ async def extract_device_control_params():
                 elif param["id_pointkey"] == "WMax":
                     power_limit = param["value"]
                 elif param["id_pointkey"] == "WMaxPercent":
-                    power_limit_percent_temp = param["value"] or int((power_limit / rated_power_custom_calculator) * 100)
+                    if power_limit_percent_enable :
+                        power_limit_percent_temp = param["value"] 
+                    else:
+                        power_limit_percent_temp = int((power_limit / rated_power_custom_calculator) * 100)
                 elif param["id_pointkey"] == "VarMaxPercentEnable":
                     reactive_limit_percent_enable = param["value"]
                 elif param["id_pointkey"] == "VarMax":
