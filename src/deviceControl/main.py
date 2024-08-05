13e1794bca804f1204eff1170ccf9533e2355823
duybinh <ndbinhtdh99@gmail.com>
2024-08-05 13:47:07 +0700

    Done

--- src/deviceControl/main.py
+++ src/deviceControl/main.py
@@ -390,7 +390,6 @@ async def pudSystempModeTrigerEachDeviceChange(MessageCheckModeSystemp, StringSe
             # Wait for up to 5 seconds for the data to be available
             result_checkmode_control = await MySQL_Select_v1("SELECT device_list.mode ,device_list.id FROM device_list JOIN device_type ON device_list.id_device_type = device_type.id WHERE device_type.name = 'PV System Inverter' AND device_list.status = 1;")
             modes = set([item['mode'] for item in result_checkmode_control])
-            
             if len(modes) == 1:
                 if 0 in modes:
                     data_send = {"id_device": "Systemp", "mode": 0}
@@ -1460,7 +1459,7 @@ async def processMessage(topic, message,StringSerialNumerInTableProjectSetup, ho
         elif topic in [topic8,topic9]:
             print("result_topic8",result_topic8)
             # If there is no timeout, there will be confusion between message man and message auto
-            await asyncio.sleep(2)
+            await asyncio.sleep(3)
             result_topic8 = message
             await pudSystempModeTrigerEachDeviceChange(result_topic8,StringSerialNumerInTableProjectSetup, host, port, username, password)
             print("result_topic8",result_topic8)
