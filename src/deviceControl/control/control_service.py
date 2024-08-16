8626b5dfb45195fb423f2300ffe7b7f32d114207
duybinh <ndbinhtdh99@gmail.com>
2024-08-16 14:28:06 +0700

    Done

--- src/deviceControl/control/control_service.py
+++ src/deviceControl/control/control_service.py
@@ -69,33 +69,43 @@ async def updateDeviceMode(mode):
 async def handle_zero_export_mode(message):
     ValueOffsetTemp = 0
     ValueThresholdTemp = 0
-    ResultQuery = []
+    ResultQuery = None
     # Get ValueOffset From Message
     ValueOffsetTemp = message.get("offset")
     if ValueOffsetTemp is not None:
         ValueOffset = ValueOffsetTemp
+    else:
+        ValueOffset = None
     # Get ValueThreshold From Message
     ValueThresholdTemp = message.get("threshold")
     if ValueThresholdTemp is not None:
         ValueThreshold = ValueThresholdTemp
+    else:
+        ValueThreshold = None
     # Result Query 
-    ResultQuery = MySQL_Update_V1("update project_setup set value_offset_zero_export = %s, threshold_zero_export = %s", (ValueOffset, ValueThreshold))
+    if ValueOffset is None or ValueThreshold is None :
+        ResultQuery = MySQL_Update_V1("update project_setup set value_offset_zero_export = %s, threshold_zero_export = %s", (ValueOffset, ValueThreshold))
     return ValueOffset,ValueThreshold,ResultQuery
 
 async def handle_power_limit_mode(message,TotalPower):
     ValueOffsetTemp = 0
     ValuePowerLimitTemp = 0
-    ResultQuery = []
+    ResultQuery = None
     # Get ValueOffset From Message
     ValueOffsetTemp = message.get("offset")
     if ValueOffsetTemp is not None:
         ValueOffset = ValueOffsetTemp
+    else:
+        ValueOffset = None 
     # Get ValuePowerLimit From Message
     ValuePowerLimitTemp = message.get("value")
     if ValuePowerLimitTemp is not None and ValuePowerLimitTemp <= TotalPower:
         ValuePowerLimit = ValuePowerLimitTemp
         ValuePowerLimit = ValuePowerLimit - (ValuePowerLimit * ValueOffset) / 100
+    else:
+        ValuePowerLimit = None 
         # Result Query 
+    if ValueOffset is None or ValuePowerLimit is None :
         ResultQuery =  MySQL_Update_V1("update project_setup set value_power_limit = %s, value_offset_power_limit = %s", (ValuePowerLimitTemp, ValueOffset))
     return ValueOffset,ValuePowerLimit,ResultQuery
 
