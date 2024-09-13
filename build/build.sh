20daa17bb11ef5fc355310143f7f816d76255113
nartnahnnw <160694961+nartnahnnw@users.noreply.github.com>
2024-09-13 09:43:20 +0700

    Add build services

---
+++ build/build.sh
@@ -0,0 +1,14 @@
+cd ../
+if [ ! -d .venv ]; then
+    python3 -m venv .venv
+    source .venv/bin/activate
+else
+    source .venv/bin/activate
+
+for d in */; do
+    echo "Installing requirements for $d"
+    if [ -f "$d"/requirements.txt ]; then
+        pip install -r "$d"/requirements.txt
+    fi
+done
+fi
---
+++ setup.py
@@ -0,0 +1,31 @@
+import subprocess
+import sys
+from pathlib import Path
+
+PATH = Path(__file__).parent.absolute()
+
+pids = ["DEVICE_SERVICE", "SINGLE_LINE_DIAGRAM_SERVICE"]
+
+
+def init(path: str, pid: str):
+    abs_dirname = PATH
+    subprocess.Popen(
+        f'sudo pm2 start {abs_dirname}/{path}/main.py --interpreter {abs_dirname}/venv/bin/python3 -f --name "{pid}" '
+        f'--restart-delay=10000', shell=True).communicate()
+
+
+def clear(pid: str):
+    # use run with ubuntu/linux
+    subprocess.Popen(
+        f'sudo pm2 delete {pid}', shell=True).communicate()
+
+
+if __name__ == '__main__':
+    if sys.platform == 'win32':
+        print("Windows is not supported")
+        sys.exit()
+
+    for pid in pids:
+        clear(pid)
+        init(pid.lower(), pid)
+
