f3caec8e63dce5c071f3a7bad288c11b846c7e29
nartnahnnw <160694961+nartnahnnw@users.noreply.github.com>
2024-09-13 09:48:54 +0700

    Update build.sh

--- build/build.sh
+++ build/build.sh
@@ -1,4 +1,5 @@
 cd ../
+export server="test"
 if [ ! -d .venv ]; then
     python3 -m venv .venv
     source .venv/bin/activate
@@ -9,6 +10,7 @@ for d in */; do
     echo "Installing requirements for $d"
     if [ -f "$d"/requirements.txt ]; then
         pip install -r "$d"/requirements.txt
+        cp "$d"/".env.example.$server" "$d"/.env
     fi
 done
 fi
