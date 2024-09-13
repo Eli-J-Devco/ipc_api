2b7f66cb67fc0a0182fa658879a4f0e0f2cd60b7
nartnahnnw <160694961+nartnahnnw@users.noreply.github.com>
2024-09-13 10:32:32 +0700

    Update build.sh

--- build/build.sh
+++ build/build.sh
@@ -10,6 +10,7 @@ for d in */; do
     echo "Installing requirements for $d"
     if [ -f "$d"/requirements.txt ]; then
         pip install -r "$d"/requirements.txt
+        echo "Creating .env file for $d/.env.$server.sample"
         cp "$d"/".env.$server.sample" "$d"/.env
     fi
 done
